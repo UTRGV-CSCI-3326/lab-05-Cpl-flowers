3dd812bdc2170230e726356fcf4e41dc997e3aa1
Cpl-flowers <126724988+Cpl-flowers@users.noreply.github.com>
2025-02-02 02:50:18 +0000

    First Commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Input {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        String patient;
+        Integer age;
+        Float weight;
+        Boolean smoker = false;
+
+        System.out.print("Enter Patient's name: ");
+        patient = scanner.nextLine();
+
+        System.out.print("Enter Patient's age: ");
+        age = scanner.nextInt();
+
+        System.out.print("Enter Patient's weight: ");
+        weight = scanner.nextFloat();
+
+        System.out.print("Enter if Patient smokes: ");
+        smoker = scanner.nextBoolean();
+
+
+        System.out.print("\n" + "Patient Information" + "\n" + "Patient's name: " + patient + "\n" + "Patient's age: " + age + "\n" + "Patient's weight: " + weight + "\n" + "Smoker? " + smoker + "\n");
+
+    }
+}
